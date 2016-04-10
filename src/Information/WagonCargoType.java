eeb68f3190c1288e3f07d302f4a8e50b9d939666
Henrik Nørhede <henrik@norhede.dk>
2016-04-10 17:22:39 +0200

    WagonWeight

--- src/Information/WagonCargoType.java
+++ src/Information/WagonCargoType.java
@@ -10,7 +10,7 @@ public class WagonCargoType
     static String[] cargoTypeList = {"Wood", "Steel", "Oil", "Gasoline", "Food", "Cars", "General goods"};
     private static final int LENGTH = 7;
 
-    public static String getDestination() {
+    public static String getCargoType() {
         Random random = new Random();
         int n = random.nextInt(LENGTH);
         String cargoType = cargoTypeList[n];
