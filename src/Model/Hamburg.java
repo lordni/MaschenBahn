7f0501abcd30e7a676dc315a1c1519b2117f5fcf
Troels <godkodedk@gmail.com>
2016-04-10 18:01:41 +0200

    First 100 wagons

--- src/Model/Hamburg.java
+++ src/Model/Hamburg.java
@@ -1,9 +1,19 @@
 package Model;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * Created by Henrik on 10-04-2016.
  */
 public class Hamburg
 {
+    Queue<Wagon> trackArrival= new LinkedList<>();
+
     // Creates all of our tracks
+    public Hamburg(){
+        for (int i = 0; i < 100; i++){
+            trackArrival.offer(new Wagon());
+        }
+    }
 }
--- src/Model/Wagon.java
+++ src/Model/Wagon.java
@@ -1,9 +1,15 @@
 package Model;
 
+import Information.Destination;
+
 /**
  * Created by Henrik on 10-04-2016.
  */
 public class Wagon
 {
+    private String destination;
     // Creates all of our wagons
+    public Wagon(){
+        destination = Destination.getDestination();
+    }
 }
